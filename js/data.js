8d9a5405a24f75085881b205170285984591c7b8
Enrique Juan de Dios <juandedioz@gmail.com>
2018-08-25 12:38:40 -0500

    Setup game with success answers

--- js/data.js
+++ js/data.js
@@ -2,215 +2,160 @@ GAME.data = [
   // id | name | website | twitter | topic
   {
     id: 1,
-    name: 'Alex Giron',
-    website: 'cssbeauty.com',
-    twitter: 'alexgiron',
-    topic: 'Designing with interactions to interacting with designs',
     question: '¿Cuándo se fundó la marca Charry Studio?',
     answer: 1,
     options: ['2011', '2013', '2015']
   },
   {
     id: 2,
-    name: 'Alex Russell',
-    website: 'infrequently.org',
-    twitter: 'slightlylate',
-    topic: 'Web: platform or pile ’o hacks?',
     question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    answer: 3,
+    options: [
+      'Maquillaje para novias	',
+      'Artístico',
+      'Automaquillaje y Maquillaje Corporativo'
+    ]
   },
   {
     id: 3,
-    name: 'Chris Coyier',
-    website: 'css-tricks.com',
-    twitter: 'chriscoyier',
-    topic: 'Awesome technologies hanging out together',
-    question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿Cuál es nuestra colección más vendida?	',
+    answer: 3,
+    options: ['Sparkle Pearl', 'Goddess Pink	', 'Deluxe Edition']
   },
   {
     id: 4,
-    name: 'David DeSandro',
-    website: 'desandro.com',
-    twitter: 'desandro',
-    topic: 'Mobile Safari: bring your machete',
-    question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿Qué tipo de hebras tiene la colección Deluxe Edition?',
+    answer: 3,
+    options: [
+      'Sólo hebra natural	',
+      'Sólo fibra sintética',
+      'Tiene hebra natural, fibra sintética y doble fibra'
+    ]
   },
   {
     id: 5,
-    name: 'Divya Manian',
-    website: 'nimbupani.com',
-    twitter: 'divya',
-    topic: 'Designing in the browser',
-    question: '¿Qué tipo de hebras tiene la colección Deluxe Edition?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿A quién dedica la colección Goddess Pink?',
+    answer: 2,
+    options: [
+      'A personas tradicionales',
+      'A personas dulces, optimistas, soñadoras, de espíritu joven y encantador.',
+      'A personas vanguardistas'
+    ]
   },
   {
     id: 6,
-    name: 'Bartosz Szopka',
-    website: 'bartaz.github.com',
-    twitter: 'bartaz',
-    topic: 'Learning to teach',
-    question: '¿A quién dedica la colección Goddess Pink?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
-  },
-  {
-    id: 7,
-    name: 'Harry Roberts',
-    website: 'csswizardry.com',
-    twitter: 'csswizardry',
-    topic: 'Breaking good habits',
     question:
       'Actualmente, ¿Qué tipo de productos profesionales diseña Charry Studio?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: [
+      'Exclusivamente brocha de maquillaje',
+      'Paleta de sombras',
+      'Lipsticks'
+    ]
+  },
+  {
+    id: 7,
+    question: '¿Cuál es nuestra colección ultra-básica?',
+    answer: 1,
+    options: ['Sparkle Pearl', 'Goddess Pink	', 'Deluxe Edition']
   },
   {
     id: 8,
-    name: 'Jina Bolton',
-    website: 'sushiandrobots.com',
-    twitter: 'jina',
-    topic: 'Style guide-driven UI design with Sass',
-    question: '¿Cuál es nuestra colección ultra-básica?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿Cuál es la colección que tiene el logotipo de fino tallado?',
+    answer: 2,
+    options: ['Sparkle Pearl', 'Goddess Pink	', 'Deluxe Edition']
   },
   {
     id: 9,
-    name: 'Julien Gachadoat',
-    website: 'v3ga.net',
-    twitter: 'v3ga',
-    topic: 'Mind the gap',
-    question: '¿Cuál es la colección que tiene el logotipo de fino tallado?',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question:
+      '¿Qué colecciones de brochas viene con un Brush Holder (portabrochas)',
+    answer: 3,
+    options: [
+      'Deluxe Edition y Goddess Pink',
+      'Goddess Pink',
+      'Sparkle Pearl y Deluxe Edition'
+    ]
   },
   {
     id: 10,
-    name: 'Krzysztof Szafranek',
-    website: 'szafranek.net',
-    twitter: 'szafranek',
-    topic: 'Refactoring Javascript – big bang theory?',
     question:
-      '¿Qué colecciones de brochas viene con un Brush Holder (portabrochas)',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+      '¿Cuándo la firma peruana lanza sus dos primeras líneas de brochas propias?',
+    answer: 3,
+    options: [
+      'Un año después de su fundación ',
+      'Dos años después de su fundación',
+      'Tres años después de su fundación'
+    ]
   },
   {
     id: 11,
-    name: 'Kyle Simpson',
-    website: 'getify.com',
-    twitter: 'getify',
-    topic: 'HTML5 Javascript on crack',
-    question:
-      '¿Cuándo la firma peruana lanza sus dos primeras líneas de brochas propias?',
+    question: '¿Cuánto % de descuento tienes en tu primera compra online?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['10%', '5%', 'Ninguno']
   },
   {
     id: 12,
-    name: 'Lea Verou',
-    website: 'lea.verou.me',
-    twitter: 'leaverou',
-    topic: 'CSS in the 4th dimension: not your daddy’s CSS animations',
-    question: '',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿Quién lidera la marca Charry Studio?	',
+    answer: 2,
+    options: ['Roxana Chavarry', 'Rosse Chavarry', 'Rosario Chavarry']
   },
   {
     id: 13,
-    name: 'Mathias Bynens',
-    website: 'mathiasbynens.be',
-    twitter: 'mathias',
-    topic: '10 things I didn’t know about HTML',
-    question: '',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿Qué tipo de color Pantone utiliza la colección Goddess Pink?',
+    answer: 3,
+    options: ['Pantone 250 C.	', 'Pantone 230 C.	', 'Pantone 210 C.']
   },
   {
     id: 14,
-    name: 'Olov Lassus',
-    website: 'lassus.se',
-    twitter: 'olov',
-    topic: 'JavaScript: the subsets we use',
-    question: '',
+    question: '¿Cómo se llama la última colección de brochas de Charry Studio?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['Goddess Pink', 'Goddess Violet', 'Goddess Gold']
   },
   {
     id: 15,
-    name: 'Rachel Andrew',
-    website: 'rachelandrew.co.uk',
-    twitter: 'rachelandrew',
-    topic: 'Pushing the boundaries without breaking the web',
-    question: '',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿De qué color son las brochas de la colección Deluxe Edition?',
+    answer: 3,
+    options: ['Rosa', 'Blancas', 'Negras']
   },
   {
     id: 16,
-    name: 'Rebecca Murphey',
-    website: 'rebeccamurphey.com',
-    twitter: 'rmurphey',
-    topic: 'Patterns for organizing non-trivial Javascript applications',
-    question: '',
-    answer: 1,
-    options: ['2011', '2013', '2015']
+    question: '¿De qué color son las brochas de la colección Sparkle Pearl?',
+    answer: 2,
+    options: ['Rosa', 'Blancas', 'Negras']
   },
   {
     id: 17,
-    name: 'Tali Garsiel',
-    website: 'taligarsiel.com',
-    twitter: '',
-    topic: 'How browsers work internally',
-    question: '',
+    question: '¿De qué color son las brochas de la colección Goddess Pink?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['Rosa', 'Blancas', 'Negras']
   },
   {
     id: 18,
-    name: 'Tiffany Conroy',
-    website: 'tif.ca',
-    twitter: 'theophani',
-    topic: 'Design processes, not interfaces',
-    question: '',
+    question: '¿Con quién recibió Rosse Chavarry una clase maestra?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['Miguel Urtecho	', 'Marcelo Wong', 'Fito Espinoza']
   },
   {
     id: 19,
-    name: 'Vitaly Friedman',
-    website: 'smashingmagazine.com',
-    twitter: 'smashingmag',
-    topic: 'Responsive redesign: Smashing Magazine’s case-study',
-    question: '',
+    question:
+      '¿Qué colecciones de brochas viene con 12 piezas profesionales para rostro, ojos y labios?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['Deluxe Edition', 'Goddess Pink', 'Sparkle Pearl']
   },
   {
     id: 20,
-    name: 'John Schulz',
-    website: 'JFSIII.org',
-    twitter: 'JFSIII',
-    topic: 'When to Node?',
-    question: '',
+    question: '¿Qué carrera profesional siguió Rosse Chavarry?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: ['Ciencias de la comunicación	', 'Periodismo', 'Diseño de modas']
   },
   {
     id: 21,
-    name: 'Luca Sale',
-    website: 'blackberry.com',
-    twitter: 'blackberryluca',
-    topic: 'Alice.js: A Lightweight Independent CSS Engine',
-    question: '',
+    question: '¿Qué premios te puedes llevar si ganas este juego online?',
     answer: 1,
-    options: ['2011', '2013', '2015']
+    options: [
+      '1 Set de la colección: Goddess Pink, Deluxe Edition o Sparkle Pearl	',
+      'Clase de automaquillaje	',
+      'Premio sorpresa	'
+    ]
   }
 ];
