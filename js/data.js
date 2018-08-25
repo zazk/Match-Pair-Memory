802a0a3232034589b83f0bd8cc1cd530968b4bc2
Enrique Juan de Dios <juandedioz@gmail.com>
2018-08-25 12:18:22 -0500

    First Setup. Show right answer

---
+++ js/data.js
@@ -0,0 +1,216 @@
+GAME.data = [
+  // id | name | website | twitter | topic
+  {
+    id: 1,
+    name: 'Alex Giron',
+    website: 'cssbeauty.com',
+    twitter: 'alexgiron',
+    topic: 'Designing with interactions to interacting with designs',
+    question: '¿Cuándo se fundó la marca Charry Studio?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 2,
+    name: 'Alex Russell',
+    website: 'infrequently.org',
+    twitter: 'slightlylate',
+    topic: 'Web: platform or pile ’o hacks?',
+    question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 3,
+    name: 'Chris Coyier',
+    website: 'css-tricks.com',
+    twitter: 'chriscoyier',
+    topic: 'Awesome technologies hanging out together',
+    question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 4,
+    name: 'David DeSandro',
+    website: 'desandro.com',
+    twitter: 'desandro',
+    topic: 'Mobile Safari: bring your machete',
+    question: 'En servicios, ¿Qué cursos brinda Charry Studio?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 5,
+    name: 'Divya Manian',
+    website: 'nimbupani.com',
+    twitter: 'divya',
+    topic: 'Designing in the browser',
+    question: '¿Qué tipo de hebras tiene la colección Deluxe Edition?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 6,
+    name: 'Bartosz Szopka',
+    website: 'bartaz.github.com',
+    twitter: 'bartaz',
+    topic: 'Learning to teach',
+    question: '¿A quién dedica la colección Goddess Pink?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 7,
+    name: 'Harry Roberts',
+    website: 'csswizardry.com',
+    twitter: 'csswizardry',
+    topic: 'Breaking good habits',
+    question:
+      'Actualmente, ¿Qué tipo de productos profesionales diseña Charry Studio?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 8,
+    name: 'Jina Bolton',
+    website: 'sushiandrobots.com',
+    twitter: 'jina',
+    topic: 'Style guide-driven UI design with Sass',
+    question: '¿Cuál es nuestra colección ultra-básica?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 9,
+    name: 'Julien Gachadoat',
+    website: 'v3ga.net',
+    twitter: 'v3ga',
+    topic: 'Mind the gap',
+    question: '¿Cuál es la colección que tiene el logotipo de fino tallado?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 10,
+    name: 'Krzysztof Szafranek',
+    website: 'szafranek.net',
+    twitter: 'szafranek',
+    topic: 'Refactoring Javascript – big bang theory?',
+    question:
+      '¿Qué colecciones de brochas viene con un Brush Holder (portabrochas)',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 11,
+    name: 'Kyle Simpson',
+    website: 'getify.com',
+    twitter: 'getify',
+    topic: 'HTML5 Javascript on crack',
+    question:
+      '¿Cuándo la firma peruana lanza sus dos primeras líneas de brochas propias?',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 12,
+    name: 'Lea Verou',
+    website: 'lea.verou.me',
+    twitter: 'leaverou',
+    topic: 'CSS in the 4th dimension: not your daddy’s CSS animations',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 13,
+    name: 'Mathias Bynens',
+    website: 'mathiasbynens.be',
+    twitter: 'mathias',
+    topic: '10 things I didn’t know about HTML',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 14,
+    name: 'Olov Lassus',
+    website: 'lassus.se',
+    twitter: 'olov',
+    topic: 'JavaScript: the subsets we use',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 15,
+    name: 'Rachel Andrew',
+    website: 'rachelandrew.co.uk',
+    twitter: 'rachelandrew',
+    topic: 'Pushing the boundaries without breaking the web',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 16,
+    name: 'Rebecca Murphey',
+    website: 'rebeccamurphey.com',
+    twitter: 'rmurphey',
+    topic: 'Patterns for organizing non-trivial Javascript applications',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 17,
+    name: 'Tali Garsiel',
+    website: 'taligarsiel.com',
+    twitter: '',
+    topic: 'How browsers work internally',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 18,
+    name: 'Tiffany Conroy',
+    website: 'tif.ca',
+    twitter: 'theophani',
+    topic: 'Design processes, not interfaces',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 19,
+    name: 'Vitaly Friedman',
+    website: 'smashingmagazine.com',
+    twitter: 'smashingmag',
+    topic: 'Responsive redesign: Smashing Magazine’s case-study',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 20,
+    name: 'John Schulz',
+    website: 'JFSIII.org',
+    twitter: 'JFSIII',
+    topic: 'When to Node?',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  },
+  {
+    id: 21,
+    name: 'Luca Sale',
+    website: 'blackberry.com',
+    twitter: 'blackberryluca',
+    topic: 'Alice.js: A Lightweight Independent CSS Engine',
+    question: '',
+    answer: 1,
+    options: ['2011', '2013', '2015']
+  }
+];
---
+++ js/game.js
@@ -0,0 +1,285 @@
+GAME.Init = function() {
+  // shuffle the input data so it looks kinda random
+  for (var i = 10; i >= 0; i--) {
+    GAME.shuffleArray(GAME.data);
+  }
+  GAME._counter = 10;
+  GAME.$id('start').onclick = function() {
+    if (!GAME._active) GAME.Start();
+  };
+  GAME.$id('howto').onclick = function() {
+    GAME.Page('howto');
+  };
+  GAME.$id('about').onclick = function() {
+    GAME.Page('about');
+  };
+  //GAME.$id('threedee').onclick = function() { GAME.ThreeD(); };
+  GAME.$id('geo').onclick = function() {
+    GAME.API.geolocation();
+  };
+  GAME.$id('ticket').onclick = function() {
+    GAME.$showModal(GAME.$txt.winticket + GAME.$txt.close, 'winticket');
+  };
+  var actualScore = GAME.API.localStorage('showScore');
+  GAME.$id('score').onclick = function() {
+    var str = GAME.$txt.showscore;
+    str = str.replace('[0P]', actualScore.points);
+    str = str.replace('[0%]', actualScore.points / 2);
+    str = str.replace('[0M]', actualScore.minutes);
+    str = str.replace('[0S]', actualScore.seconds);
+    str = str.replace('[0C]', actualScore.trials);
+    GAME.$showModal(str + GAME.$txt.close, 'showscore');
+  };
+};
+
+GAME.Start = function(opt) {
+  GAME.$id('board').className = 'board';
+  if (opt == '3d') {
+    GAME.NewLevel(0);
+  } else {
+    GAME.NewLevel(1);
+  }
+  if (GAME._time) {
+    GAME.TimerStop();
+  }
+  GAME._time = 0;
+  GAME.Timer();
+  GAME._active = true;
+  GAME._visible = [];
+  GAME._points = 0;
+  GAME._trials = 0;
+  GAME._questions = 0;
+
+  GAME.$id('time').innerHTML = '00:00';
+  GAME.$id('points').innerHTML = '0';
+  GAME.$id('trials').innerHTML = '0';
+  GAME.$id('questions').innerHTML = '0';
+};
+
+GAME.NewLevel = function(lvl) {
+  // generate HTML structure of the game board
+  var board = GAME.$id('board');
+  for (var i = 0, elements = ''; i < GAME._counter * 2; i++) {
+    elements +=
+      '<p id="card_' +
+      i +
+      '"><span class="front" id="front_' +
+      i +
+      '"></span><span class="back"></span></p>';
+  }
+
+  // bind clicks to the cards
+  if (lvl) {
+    board.innerHTML = elements;
+  } else {
+    lvl = 1;
+    for (var i = 0; i < 5; i++) {
+      var cardHTML = '';
+      for (var j = 0; j < 4; j++) {
+        cardHTML += '<p id="card_' + (i * 4 + j) + '"></p>';
+      }
+      GAME.$id('side_' + i).innerHTML = cardHTML; //'<p id="card_'+(i*4)+'"></p><p id="card_'+(i*4+1)+'"></p><p id="card_'+(i*4+2)+'"></p><p id="card_'+(i*4+3)+'"></p>';
+    }
+  }
+
+  var els = board.getElementsByTagName('p');
+  for (var i = els.length - 1; i >= 0; i--) {
+    els[i].onclick = function() {
+      GAME.CardClick(this);
+    };
+  }
+
+  GAME._board = [];
+  // take the items from the data table and add them twice
+  for (var i = 0; i < GAME._counter; i++) {
+    var data = GAME.data[(lvl - 1) * GAME._counter + i];
+    GAME._board.push(data, data);
+  }
+
+  // shuffle array 10 times - just to be sure it's more or less random
+  for (var i = 10; i >= 0; i--) {
+    GAME.shuffleArray(GAME._board);
+  }
+};
+
+GAME.HideCards = function() {
+  var item1 = GAME._visible.pop(),
+    item2 = GAME._visible.pop(),
+    myItem1 = GAME.$id('card_' + item1.card_id),
+    myItem2 = GAME.$id('card_' + item2.card_id);
+  myItem1.className = '';
+  myItem2.className = '';
+  GAME._active = true;
+  if (GAME.$id('front_' + item1.card_id)) {
+    GAME.$id('front_' + item1.card_id).style.backgroundPosition = '0px 0px';
+    GAME.$id('front_' + item2.card_id).style.backgroundPosition = '0px 0px';
+    GAME.$id('front_' + item1.card_id).style.zIndex = '-1';
+    GAME.$id('front_' + item2.card_id).style.zIndex = '-1';
+  }
+  myItem1.onclick = function() {
+    GAME.CardClick(this);
+  };
+  myItem2.onclick = function() {
+    GAME.CardClick(this);
+  };
+};
+
+GAME.DisableCards = function() {
+  var item1 = GAME._visible.pop(),
+    item2 = GAME._visible.pop(),
+    myItem1 = GAME.$id('card_' + item1.card_id),
+    myItem2 = GAME.$id('card_' + item2.card_id);
+  myItem1.className = 'disabled';
+  myItem1.onclick = function() {};
+  myItem2.className = 'disabled';
+  myItem2.onclick = function() {};
+  GAME._active = true;
+};
+
+GAME.CardClick = function(card) {
+  if (GAME._active) {
+    var card_id = card.id.split('_')[1],
+      item = GAME._board[card_id];
+
+    card.onclick = function() {};
+    card.className = 'visible';
+    card.style.backgroundPosition = '-' + item.id * 100 + 'px 0';
+    if (GAME.$id('front_' + card_id)) {
+      GAME.$id('front_' + card_id).style.backgroundPosition =
+        '-' + item.id * 100 + 'px 0';
+      GAME.$id('front_' + card_id).style.zIndex = '10';
+    }
+    GAME._visible.push({ item_id: item.id, card_id: card_id });
+    if (GAME._visible.length == 2) {
+      // two visible cards
+      if (GAME._visible[0].item_id == GAME._visible[1].item_id) {
+        // the same card ID
+        // show the form with the question
+        setTimeout(GAME.DisableCards, 1);
+        GAME.Form(item);
+      } else {
+        setTimeout(GAME.HideCards, 1000);
+      }
+      GAME._active = false;
+      GAME._trials += 1;
+      GAME.$id('trials').innerHTML = GAME._trials;
+    }
+  }
+};
+
+GAME.Form = function(item) {
+  var answerTable = [...item.options];
+  // push one proper answer...
+
+  GAME.shuffleArray(answerTable);
+
+  var formHTML = GAME.$form(item, item.question, answerTable);
+  GAME.$showModal(formHTML);
+  GAME.$id('radio1').onclick = function() {
+    GAME.CheckAnswer(this, item.options[item.answer - 1]);
+  };
+  GAME.$id('radio2').onclick = function() {
+    GAME.CheckAnswer(this, item.options[item.answer - 1]);
+  };
+  GAME.$id('radio3').onclick = function() {
+    GAME.CheckAnswer(this, item.options[item.answer - 1]);
+  };
+  GAME.$id('continue').style.visibility = 'hidden';
+  //document.getElementsByTagName('input').onclick = function() { GAME.CheckAnswer(this); };
+};
+
+GAME.CheckAnswer = function(chosen, correct) {
+  if (chosen.value == correct) {
+    GAME.$id('message').innerHTML = GAME.$txt.correct;
+    GAME.$id('message').style.color = 'green';
+    GAME.$id('message').style.fontWeight = 'bold';
+    //chosen.style.background = 'green';
+    GAME._points += 10;
+    GAME.$id('points').innerHTML = GAME._points;
+    GAME.$id(chosen.id + 'label').style.color = 'green';
+    GAME.$id(chosen.id + 'label').innerHTML; // += ' ✓';
+  } else {
+    GAME.$id('message').innerHTML = GAME.$txt.wrong;
+    GAME.$id('message').style.color = 'red';
+    GAME.$id('message').style.fontWeight = 'bold';
+    GAME.$id(chosen.id + 'label').style.color = 'red';
+    GAME.$id(chosen.id + 'label').innerHTML; // += ' ✗';
+  }
+
+  GAME._questions += 1;
+  GAME.$id('questions').innerHTML = GAME._questions;
+  GAME.$id('continue').style.visibility = 'visible';
+  GAME.$id('radio1').disabled = true;
+  GAME.$id('radio2').disabled = true;
+  GAME.$id('radio3').disabled = true;
+  GAME.$id('form').className = 'disabled';
+  //	GAME.$id(correct.id+'label').style.color = 'green';
+
+  GAME.$id('continue').onclick = function() {
+    GAME.$hideModal();
+    if (GAME._questions == 10) {
+      setTimeout(function() {
+        GAME.$showModal(GAME.$txt.halfway + GAME.$txt.continue, 'halfway');
+        GAME.$id('newLevel').style.visibility = 'visible';
+        GAME.$id('newLevel').onclick = function() {
+          GAME.$hideModal();
+          GAME.NewLevel(2);
+        };
+      }, 200);
+    } else if (GAME._questions == 20) {
+      setTimeout(function() {
+        GAME.TimerStop();
+        GAME.Page('gameover');
+      }, 200);
+    }
+  };
+};
+
+GAME.Timer = function() {
+  var sec = ~~(GAME._time % 60),
+    min = ~~(GAME._time / 60),
+    secHTML = sec < 10 ? '0' + sec : sec,
+    minHTML = min < 10 ? '0' + min : min;
+  GAME.$id('time').innerHTML = minHTML + ':' + secHTML;
+  GAME._time++;
+  GAME.TimerClock = setTimeout('GAME.Timer()', 1000);
+};
+
+GAME.TimerStop = function() {
+  clearTimeout(GAME.TimerClock);
+};
+
+GAME.Page = function(page) {
+  if (page == 'gameover') {
+    // HTML5 OFFLINE
+    if (navigator.onLine) {
+      // serve normal stuff
+    } else {
+      // put info that you're offline and can't tweet
+    }
+
+    // HTML5 LOCAL STORAGE
+    GAME.API.localStorage('saveScore');
+
+    var minHTML = ~~(GAME._time / 60),
+      sec = GAME._time % 60,
+      secHTML = sec < 10 ? '0' + sec : sec,
+      timeHTML = minHTML + ':' + secHTML;
+
+    var str = GAME.$txt.gameover;
+    str = str.replace('[0P]', GAME._points);
+    str = str.replace('[TWEET]', GAME.$txt.tweet);
+    str = str.replace('[0P]', GAME._points);
+    str = str.replace('[0%]', GAME._points / 2);
+    str = str.replace('[0T]', timeHTML);
+    str = str.replace('[0M]', minHTML);
+    str = str.replace('[0S]', secHTML);
+    GAME.$showModal(str, 'gameover');
+  } else {
+    GAME.$showModal(
+      '' + GAME.$id('page-' + page).innerHTML + GAME.$txt.close,
+      'page',
+      'page-' + page
+    );
+  }
+};
